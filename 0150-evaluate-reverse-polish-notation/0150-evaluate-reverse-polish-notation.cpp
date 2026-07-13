5268452678f00a016f096c3860e3019df4c0c54b
Vaibhav Saini <sainivaibhav1409@gmail.com>
2026-07-13 16:17:52 +0530

    Time: 3 ms (35.92%), Space: 17.4 MB (8.77%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,22 +1,51 @@
 class Solution {
 public:
-    int Operate(int a , int b , string token){
-        if(token == "+") 
-        return a + b;
+    // int Operate(int a , int b , string token){
+    //     if(token == "+") 
+    //     return a + b;
 
-        if(token == "-") 
-        return a - b;
+    //     if(token == "-") 
+    //     return a - b;
 
-        if(token == "*") 
-        return a * b;
+    //     if(token == "*") 
+    //     return a * b;
 
-        if(token == "/") 
-        return a / b;
+    //     if(token == "/") 
+    //     return a / b;
 
-        return -1;  
-    }
-    int evalRPN(vector<string>& tokens) {
+    //     return -1;  
+    // }
+    // int evalRPN(vector<string>& tokens) {
+    //     stack<int> st;
+    //     for(string &token : tokens ){
+    //         if(token == "+" || token == "-" || token == "*" || token == "/"){
+    //             //top 2 elements ko Nikalkar operate krlo
+    //             // fir push krdo stack m
+    //             int b = st.top();
+    //             st.pop();
+    //             int a = st.top();
+    //             st.pop();
+
+    //             int res = Operate(a , b , token);
+    //             st.push(res);
+    //         }  
+    //         else {
+    //             st.push(stoi(token)); // string ko int m convert krdega
+    //         }
+    //     }
+    //     return st.top();
+    // }
+
+    // SOLVING BY unordered_map to remove Operate function
+     int evalRPN(vector<string>& tokens) {
         stack<int> st;
+        unordered_map<string , function<int (int , int)>> mp = {
+            {"+" , [](int a , int b) {return a+b;}},
+            {"-" , [](int a , int b) {return a-b;}},
+            {"*" , [](int a , int b) {return a*b;}},
+            {"/" , [](int a , int b) {return a/b;}},
+
+        };
         for(string &token : tokens ){
             if(token == "+" || token == "-" || token == "*" || token == "/"){
                 //top 2 elements ko Nikalkar operate krlo
@@ -26,7 +55,7 @@ public:
                 int a = st.top();
                 st.pop();
 
-                int res = Operate(a , b , token);
+                int res = mp[token](a , b);
                 st.push(res);
             }  
             else {
